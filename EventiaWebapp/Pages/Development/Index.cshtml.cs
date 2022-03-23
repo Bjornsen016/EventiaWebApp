6ba852395d67f0e2dd79705ded14edffad2d70ec
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-03-23 12:31:11 +0100

    Refactoring namespaces

--- EventiaWebapp/Pages/Development/Index.cshtml.cs
+++ EventiaWebapp/Pages/Development/Index.cshtml.cs
@@ -2,34 +2,33 @@ using EventiaWebapp.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace EventiaWebapp.Pages.Development
+namespace EventiaWebapp.Pages.Development;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly Database _database;
+    private readonly IWebHostEnvironment _hostingEnvironment;
+    public bool DatabaseReseted;
+
+    public IndexModel(Database database, IWebHostEnvironment hostingEnvironment)
     {
-        private readonly Database _database;
-        private readonly IWebHostEnvironment _hostingEnvironment;
-        public bool DatabaseReseted;
+        _database = database;
+        _hostingEnvironment = hostingEnvironment;
+    }
 
-        public IndexModel(Database database, IWebHostEnvironment hostingEnvironment)
-        {
-            _database = database;
-            _hostingEnvironment = hostingEnvironment;
-        }
+    public IActionResult OnGet()
+    {
+        if (!_hostingEnvironment.IsDevelopment()) return NotFound();
 
-        public IActionResult OnGet()
-        {
-            if (!_hostingEnvironment.IsDevelopment()) return NotFound();
+        return Page();
+    }
 
-            return Page();
-        }
+    public IActionResult OnPost()
+    {
+        if (!_hostingEnvironment.IsDevelopment()) return NotFound();
 
-        public IActionResult OnPost()
-        {
-            if (!_hostingEnvironment.IsDevelopment()) return NotFound();
-
-            _database.RecreateAndSeed();
-            DatabaseReseted = true;
-            return Page();
-        }
+        _database.RecreateAndSeed();
+        DatabaseReseted = true;
+        return Page();
     }
 }
