dcd6cab78415fa3c70c1357196fcb4512976f5e3
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-03-29 15:28:46 +0200

    Fixed startpage with new identity.

--- EventiaWebapp/Pages/Index.cshtml.cs
+++ EventiaWebapp/Pages/Index.cshtml.cs
@@ -1,8 +1,10 @@
 using System.Security.Claims;
 using EventiaWebapp.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace EventiaWebapp.Pages;
 
@@ -10,19 +12,23 @@ namespace EventiaWebapp.Pages;
 public class IndexModel : PageModel
 {
     private readonly Services.EventHandler _eventHandler;
+    private readonly UserManager<Models.User> _userManager;
+    private readonly SignInManager<Models.User> _signInManager;
     public Models.User? CurrentUser;
 
-    public IndexModel(Services.EventHandler eventHandler)
+    public IndexModel(Services.EventHandler eventHandler, UserManager<Models.User> userManager,
+        SignInManager<Models.User> signInManager)
     {
         _eventHandler = eventHandler;
+        _userManager = userManager;
+        _signInManager = signInManager;
     }
 
     public async Task<IActionResult> OnGetAsync()
     {
-        if (!User.Identity.IsAuthenticated) return Page();
+        if (!_signInManager.IsSignedIn(User)) return Page();
 
-        var userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-
+        var userId = _userManager.GetUserId(User);
         CurrentUser = await _eventHandler.GetUserAsync(userId);
 
         return Page();
