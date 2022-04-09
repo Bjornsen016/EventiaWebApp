388d08af72162d96f46bcba0bb7b5af54e32502e
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-04-09 22:09:14 +0200

    Added sorting to Manage Events for Orgnanizers

--- EventiaWebapp/Pages/Organizer/Index.cshtml.cs
+++ EventiaWebapp/Pages/Organizer/Index.cshtml.cs
@@ -11,6 +11,8 @@ public class IndexModel : PageModel
 {
     private readonly OrganizerHandler _organizerHandler;
     private readonly UserManager<Models.User> _userManager;
+    public string SignsSort { get; set; }
+    public string DateSort { get; set; }
     public List<Event> Events { get; set; }
 
     public IndexModel(OrganizerHandler organizerHandler, UserManager<Models.User> userManager)
@@ -19,9 +21,27 @@ public class IndexModel : PageModel
         _userManager = userManager;
     }
 
-    public async Task OnGetAsync()
+    public async Task OnGetAsync(string sortOrder)
     {
+        SignsSort = sortOrder == "signed_up" ? "signed_up_desc" : "signed_up";
+        DateSort = String.IsNullOrEmpty(sortOrder) ? "date_desc" : "";
         var userId = _userManager.GetUserId(User);
-        Events = await _organizerHandler.GetOrganizersEvents(userId);
+        var evts = await _organizerHandler.GetOrganizersEvents(userId);
+
+        switch (sortOrder)
+        {
+            case "signed_up":
+                Events = evts.OrderBy(e => e.Attendees.Count).ToList();
+                break;
+            case "signed_up_desc":
+                Events = evts.OrderByDescending(e => e.Attendees.Count).ToList();
+                break;
+            case "date_desc":
+                Events = evts.OrderByDescending(e => e.Date).ToList();
+                break;
+            default:
+                Events = evts;
+                break;
+        }
     }
 }
